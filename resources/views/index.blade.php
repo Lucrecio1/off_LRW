4a128d3a6ee094478465e6db5289af9794781776
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2024-02-17 14:52:49 +0100

    bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
+
 </head>
 <body>
     
@@ -29,9 +31,16 @@
                     </td>
                 </tr>
             @endforeach
-        <body>
-            
-        </body>
+        
     </table>
+
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,13 +1,46 @@
+
 <a href="{{route('supports.index')}}"> <<--Lista</a>
-<div>
+
+<div class="">
     <!-- Well begun is half done. - Aristotle -->
     <h1>Detalhes da Dúvida {{$support->id}}</h1>
     <ul>
+        
         <li>Assunto:{{$support->subject}}</li>
         <li>Descrição:{{$support->body}}</li>
         <li>Status:{{$support->status}}</li>
     </ul>
 </div>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
 
 <form action="{{route('supports.apagar', $support->id)}}" method="POST">
     @csrf()
